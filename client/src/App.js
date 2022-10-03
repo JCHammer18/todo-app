5d695cdc27fd35282037913a9eef4a0cf572b0c1
Charlotte Strickland <charlottemarie81@gmail.com>
2022-10-03 17:33:50 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
-
-import './App.css';
+import React from "react";
+import Tasks from "./Tasks";
+import { Paper, TextField } from "@material-ui/core";
+import { Checkbox, Button } from "@material-ui/core";
+import "./App.css";
 
 function App() {
   return (
