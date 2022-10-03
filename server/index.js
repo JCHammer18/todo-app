c9e44fdfe8a4989928c09bad2f86b2110414d77b
JCHammer18 <101356353+JCHammer18@users.noreply.github.com>
2022-10-03 17:59:58 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,6 +10,5 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/tasks", tasks);
-
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
