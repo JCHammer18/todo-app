bbe906a139b34996235c675c2dc8849adfa4e189
JCHammer18 <101356353+JCHammer18@users.noreply.github.com>
2022-10-03 17:04:20 -0400

    router post route

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -2,3 +2,11 @@ const Task = require("../models/task");
 const express = require("express");
 const router = express.Router();
 
+router.post("/", async (req, res) => {
+    try {
+        const task = await new Task(req.body).save();
+        res.send(task);
+    } catch (error) {
+        res.send(error);
+    }
+});
