fae5d0ad0595e306c93f2e82793fc2f787369a6a
JCHammer18 <101356353+JCHammer18@users.noreply.github.com>
2022-10-03 16:56:52 -0400

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,1 +1,18 @@
 const mongoose = require("mongoose");
+
+odule.exports = async () => {
+    try {
+        const connectionParams = {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+        };
+        await mongoose.connect(
+            "mongodb://localhost/todo-app",
+            connectionParams
+        );
+        console.log("Connected to database.");
+    } catch (error) {
+        console.log("Could not connect to database.", error);
+    }
+};
